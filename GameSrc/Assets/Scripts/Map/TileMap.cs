997d7182ef6a40bc3f2dc3780a346a9c8d5ba434
kolchenko.a <kolchenkoas@yandex.ru>
2018-03-29 00:10:35 +0300

    FIX: reset settings project

---
+++ GameSrc/Assets/Scripts/Map/TileMap.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+    public Transform hexPrefab;
+
+    public int tileWidth = 11;
+    public int tileHeight = 11;
+
+    float hexWidth = 1.732f;
+    float hexHeight = 2.0f;
+    public float gap = 0.0f;
+
+    Vector3 startPos;
+
+    void Start()
+    {
+        AddGap();
+        CalcStartPos();
+        CreateTile();
+    }
+
+    void AddGap()
+    {
+        hexWidth += hexWidth * gap;
+        hexHeight += hexHeight * gap;
+    }
+
+    void CalcStartPos()
+    {
+        float offset = 0;
+        if (tileHeight / 2 % 2 != 0)
+            offset = hexWidth / 2;
+
+        float x = -hexWidth * (tileWidth / 2) - offset;
+        float z = hexHeight * 0.75f * (tileHeight / 2);
+
+        startPos = new Vector3(x, 0, z);
+    }
+
+    Vector3 CalcWorldPos(Vector2 tilePos)
+    {
+        float offset = 0;
+        if (tilePos.y % 2 != 0)
+            offset = hexWidth / 2;
+
+        float x = startPos.x + tilePos.x * hexWidth + offset;
+        float z = startPos.z - tilePos.y * hexHeight * 0.75f;
+
+        return new Vector3(x, 0, z);
+    }
+
+    void CreateTile()
+    {
+        for (int y = 0; y < tileHeight; y++)
+        {
+            for (int x = 0; x < tileWidth; x++)
+            {
+                Transform hex = Instantiate(hexPrefab) as Transform;
+                Vector2 tilePos = new Vector2(x, y);
+                hex.position = CalcWorldPos(tilePos);
+                hex.parent = this.transform;
+                hex.name = "Hexagon" + x + "|" + y;
+            }
+        }
+    }
+}
